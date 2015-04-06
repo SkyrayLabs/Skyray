59e1bda4c6161559e698d299fb42e4f0a0773137
Jin Hu <bixuehujin@gmail.com>
2015-04-06 21:07:15 +0800

    Added missing tests base library

---
+++ tests/includes/SimpleEchoProtocol.php
@@ -0,0 +1,49 @@
+<?php
+use skyray\core\ProtocolInterface;
+
+
+class SimpleEchoProtocol implements ProtocolInterface
+{
+    protected $debug;
+    protected $clientMode;
+    protected $stream;
+
+    public function __construct($debug = false, $clientMode = false)
+    {
+        $this->debug = $debug;
+        $this->clientMode = $clientMode;
+    }
+
+    public function log($info)
+    {
+        echo ($this->clientMode ? '[client]: ' : '[server]: ') . $info . PHP_EOL;
+    }
+
+    public function connectStream($stream)
+    {
+        $this->stream = $stream;
+    }
+
+    public function streamConnected()
+    {
+        $this->debug && $this->log('connected');
+        if ($this->clientMode) {
+            $this->stream->write("hello");
+        }
+    }
+
+    public function dataReceived($data)
+    {
+        if ($this->clientMode) {
+            $this->log('received data: ' . $data);
+            $this->stream->close();
+        } else {
+            $this->stream->write($data);
+        }
+    }
+
+    public function streamClosed()
+    {
+        $this->debug && $this->log('closed');
+    }
+}
